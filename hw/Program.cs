46136f0d070ecc4d14565d55274f58a3148afb40
kobisa16 <kobisa16@gmail.com>
2026-05-05 19:30:37 +0300

    trgil 1 - bank

--- hw/Program.cs
+++ hw/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-           
+            Account account1 = new Account(1000, 5000);
+            account1.Withdraw(6000);
+
+            VipAccount account2 = new VipAccount(2000, 10000);
+            account2.Withdraw(12000);
+
         }
     }
+
+    class Account
+    {
+        protected int accountNumber { get; set; }
+        protected double balance { get; set; }
+
+        public Account(int accountNumber, double balance)
+        {
+            this.accountNumber = accountNumber;
+            this.balance = balance;
+        }
+        public virtual void Withdraw(double amount)
+        {
+            if (balance - amount >= 0)
+            {
+                balance -= amount;
+
+            }
+            else
+            {
+                Console.WriteLine("Not enough money");
+            }
+
+        }
+
+    }
+
 }
---
+++ hw/VipAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw
+{
+    class VipAccount : Account
+    {
+        public VipAccount(int accountNumber, double balance) : base(accountNumber, balance)
+        {
+
+        }
+        public override void Withdraw(double amount)
+        {
+            if (balance - amount >= -5000)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Not enough money");
+            }
+        }
+
+    }
+}
